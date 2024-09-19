3627cfd6c8af49e059533f7bcfa929857503e6d8
Erwan Fontaine <erwfonta@made-f0Br5s4.clusters.42paris.fr>
2024-09-19 18:31:18 +0200

    Added commentary to struct

--- includes/struct.h
+++ includes/struct.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
+/*   By: erwfonta <erwfonta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 21:51:21 by ijaber            #+#    #+#             */
-/*   Updated: 2024/09/18 23:41:57 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/09/19 18:22:46 by erwfonta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,38 @@ typedef enum s_node_type
 	NODE_OR
 }						t_node_type;
 
+/**
+ * @brief Structure representing a command.
+ * @details Used to store command information.
+ * @param name Name of the command.
+ * @param args Arguments of the command.
+ */
 typedef struct s_command
 {
 	char				*name;
 	char				**args;
 }						t_command;
 
+/**
+ * @brief Structure representing a redirection.
+ * @details Used to store redirection information.
+ * @param file Redirection file.
+ * @param type Type of redirection.
+ */
 typedef struct s_redirect
 {
 	char				*file;
 	int					type;
 }						t_redirect;
 
+/**
+ * @brief Structure representing a node in the binary tree.
+ * @details Used to store information about a node in the binary tree.
+ * @param command Pointer to a command structure.
+ * @param redirect Pointer to a redirection structure.
+ * @param left Pointer to the left node of the binary tree.
+ * @param right Pointer to the right node of the binary tree.
+ */
 typedef struct s_ast_node
 {
 	struct s_command	*command;
--- srcs/binary_tree.c
+++ srcs/binary_tree.c
@@ -1,26 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   binary_tree.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: erwfonta <erwfonta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/18 21:52:39 by ijaber            #+#    #+#             */
-/*   Updated: 2024/09/19 17:37:23 by erwfonta         ###   ########.fr       */
+/*   Created: 2024/09/19 17:33:15 by erwfonta          #+#    #+#             */
+/*   Updated: 2024/09/19 17:33:16 by erwfonta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-# include "struct.h"
-
-// 
-# include <limits.h>
-# include <stdarg.h>
-# include <stdint.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-#endif
