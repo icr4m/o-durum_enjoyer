e4bde5a7c052db5072bb62ee9843072845ae04c1
Erwan Fontaine <erwfonta@made-f0Br8s3.clusters.42paris.fr>
2024-09-24 15:21:26 +0200

    sorcier de con

--- srcs/read_line.c
+++ srcs/read_line.c
@@ -6,37 +6,11 @@
 /*   By: erwfonta <erwfonta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 23:27:21 by ijaber            #+#    #+#             */
-/*   Updated: 2024/09/24 15:25:01 by erwfonta         ###   ########.fr       */
+/*   Updated: 2024/09/24 15:25:42 by erwfonta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-// void	exec_readline(t_data *data)
-// {
-// 	char	*command_readed;
-// 	char	*prompt;
-
-// 	(void)data;
-// 	while (1)
-// 	{
-// 		setup_sig_parent();
-// 		prompt = getcwd(NULL, 0);
-// 		printf(BBLUE "→ %s " WHITE, prompt);
-// 		command_readed = readline(BRED "minishell> " WHITE);
-// 		free(prompt);
-// 		if (command_readed == NULL && g_signal_received != 0)
-// 		{
-// 			g_signal_received = 0;
-// 			continue ;
-// 		}
-// 		if (command_readed == NULL)
-// 			break ;
-// 		if (ft_strlen(command_readed) > 0)
-// 			add_history(command_readed);
-// 		free(command_readed);
-// 	}
-// }
 
 void	exec_readline(t_data *data)
 {
