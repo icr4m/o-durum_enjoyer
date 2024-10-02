cdabc6484e23aa0925ea2fa62f1a8c3bdbc99be3
Icham Jaber <ijaber@student.42.fr>
2024-10-02 22:38:40 +0200

    fix fprintf

--- srcs/built_in/cd.c
+++ srcs/built_in/cd.c
@@ -1,38 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env.c                                              :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/28 17:40:40 by ijaber            #+#    #+#             */
-/*   Updated: 2024/10/02 22:33:34 by ijaber           ###   ########.fr       */
+/*   Created: 2024/10/02 20:37:25 by ijaber            #+#    #+#             */
+/*   Updated: 2024/10/02 20:41:35 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_env(t_data *data, char **args)
+int	cd(t_data *data, char **args)
 {
-	if (args[1] != NULL)
-	{
-		ft_fprintf(2, "minishell: %s: No such file or directory", args[1]);
-		return (EXIT_FAILURE);
-	}
-	print_env(data, 1);
-	return (EXIT_SUCCESS);
-}
-
-void	print_env(t_data *data, int cas)
-{
-	t_env	*current;
-
-	current = data->env;
-	while (current)
-	{
-		if (cas == 2)
-			printf("export ");
-		printf("%s\n", current->env_var);
-		current = current->next;
-	}
 }
