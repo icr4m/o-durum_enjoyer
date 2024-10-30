b513e15acdb0c87ef4973dc26e3defe57e59f920
Icham Jaber <ijaber@student.42.fr>
2024-10-30 14:49:39 +0100

    here_doc fonctionnel mais leak

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 17:22:14 by ijaber            #+#    #+#             */
-/*   Updated: 2024/10/29 16:31:58 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/10/30 14:45:46 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,54 +18,40 @@ void	check_here_doc(t_ast_node *node, t_data *data)
 
 	if (node == NULL)
 		return ;
-	// Traiter d'abord les heredocs des sous-arbres
-	check_here_doc(node->left, data);
-	check_here_doc(node->right, data);
 	if (node->type == TOKEN_REDIR_HEREDOC)
 	{
 		fd = create_heredoc(node->right->args[0], data);
 		if (fd == -1)
 			free_and_exit(-1);
 		node->heredoc_fd = fd;
-		if (data->is_child == 0)
-		{
-			ft_close(fd);
-			node->heredoc_fd = -1;
-		}
 	}
+	check_here_doc(node->left, data);
+	check_here_doc(node->right, data);
 }
 
 static void	execute_heredoc_redirection(t_ast_node *node, t_data *data)
 {
-	int	fd;
 	int	old_stdin;
 
-	if (node->right && node->right->args[0])
+	if (data->backup_stdin == -42)
 	{
-		fd = create_heredoc(node->right->args[0], data);
-		if (fd == -1)
-			free_and_exit(-1);
-		if (data->backup_stdin == -42)
+		old_stdin = dup(STDIN_FILENO);
+		if (old_stdin == -1)
 		{
-			old_stdin = dup(STDIN_FILENO);
-			if (old_stdin == -1)
-			{
-				ft_fprintf(2, "minishell: Error when trying to dup stdin\n");
-				ft_close(fd);
-				return ;
-			}
-			data->backup_stdin = old_stdin;
-		}
-		if (dup2(fd, STDIN_FILENO) == -1)
-		{
-			ft_fprintf(2, "minishell: Error when trying to dup2\n");
-			ft_close(fd);
-			if (data->is_child == 1)
-				free_and_exit(-1);
+			ft_fprintf(2, "minishell: Error when trying to dup stdin\n");
 			return ;
 		}
-		ft_close(fd);
+		data->backup_stdin = old_stdin;
 	}
+	if (dup2(node->heredoc_fd, STDIN_FILENO) == -1)
+	{
+		ft_fprintf(2, "minishell: Error when trying to dup2\n");
+		ft_close(node->heredoc_fd);
+		if (data->is_child == 1)
+			free_and_exit(-1);
+		return ;
+	}
+	ft_close(node->heredoc_fd);
 	execute_ast(node->left, data);
 }
 
@@ -73,8 +59,7 @@ void	execute_ast(t_ast_node *node, t_data *data)
 {
 	if (node == NULL)
 		return ;
-	check_here_doc(node, data);
-	if (node->type == TOKEN_PIPE)
+	else if (node->type == TOKEN_PIPE)
 		pipe_exec(node, data);
 	else if (node->type == TOKEN_REDIR_IN)
 		handle_redirection_in(node, data);
--- srcs/exec/pipe_exec.c
+++ srcs/exec/pipe_exec.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 10:18:20 by ijaber            #+#    #+#             */
-/*   Updated: 2024/10/29 16:04:57 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/10/30 14:45:17 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,36 +17,34 @@ void	pipe_exec(t_ast_node *node, t_data *data)
 	pid_t	pid[2];
 	int		fd[2];
 	int		status;
-	int		heredoc_fd;
 
 	if (pipe(fd) == -1)
 		free_and_exit(data->status_code);
-	heredoc_fd = node->right->heredoc_fd;
 	pid[0] = ft_fork(data);
 	if (pid[0] == 0)
 	{
 		dup2(fd[1], STDOUT_FILENO);
 		(ft_close(fd[0]), ft_close(fd[1]));
-		if (node->left->heredoc_fd != -1)
-			ft_close(node->left->heredoc_fd);
 		(execute_ast(node->left, data), free_and_exit(data->status_code));
 	}
 	pid[1] = ft_fork(data);
 	if (pid[1] == 0)
 	{
-		if (heredoc_fd != -1)
-		{
-			dup2(heredoc_fd, STDIN_FILENO);
-			ft_close(heredoc_fd);
-		}
-		else
-			dup2(fd[0], STDIN_FILENO);
+		dup2(fd[0], STDIN_FILENO);
 		(ft_close(fd[0]), ft_close(fd[1]));
 		(execute_ast(node->right, data), free_and_exit(data->status_code));
 	}
+	if (node->left->heredoc_fd != -1)
+	{
+		ft_close(node->left->heredoc_fd);
+		node->left->heredoc_fd = -1;
+	}
+	if (node->right->heredoc_fd != -1)
+	{
+		ft_close(node->right->heredoc_fd);
+		node->right->heredoc_fd = -1;
+	}
 	(ft_close(fd[0]), ft_close(fd[1]));
-	if (heredoc_fd != -1)
-		ft_close(heredoc_fd);
 	waitpid(pid[0], &status, 0);
 	waitpid(pid[1], &status, 0);
 	exit_status(status, data);
